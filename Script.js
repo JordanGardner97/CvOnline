a10ce8b22d765f7ec2e81364254aebe69b4b0bba
JordanGardner97 <jordangardner20@gmail.com>
2018-12-05 16:25:59 +0000

    More Added

--- Script.js
+++ Script.js
@@ -1,3 +1,25 @@
 function sendMessageFunction(){
     alert("Your message has been sent");
+};
+
+var table = $('#CollegeResults')
+var count = 0;
+
+
+function showMoreFunction(){
+if (count ==0)
+{
+    $('#CollegeResults').css("display","block");
+    count +=1;
+    $('.Show').text("Hide Grades")
 }
+
+else
+{
+    $('#CollegeResults').css("display","none");
+    count = 0;
+    $('.Show').text("Show Grades")
+}
+
+}
+
